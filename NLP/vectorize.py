91343d2c2c35943f857e6e236ea413a46fa4ec88
drmixvm <drmixvm@DESKTOP-T3TQNU6>
2021-01-14 15:17:09 +0500

    Чтение датасета

--- NLP/vectorize.py
+++ NLP/vectorize.py
@@ -5,8 +5,8 @@ from scipy import sparse
 from tnize import GetTokens
 
 def Vectorize(corpus):
-    vector_words = TfidfVectorizer(tokenizer=GetTokens, analyzer='word', ngram_range=(1,3))
-    vector_char = TfidfVectorizer(analyzer='char', lowercase=True, ngram_range=(3,6))
+    vector_words = TfidfVectorizer(max_features=10000, tokenizer=GetTokens, analyzer='word', ngram_range=(1,3))
+    vector_char = TfidfVectorizer(max_features=30000, analyzer='char', lowercase=True, ngram_range=(3,6))
     x_words = vector_words.fit_transform(corpus)
     x_char = vector_char.fit_transform(corpus)
     return sparse.hstack([x_words, x_char])
