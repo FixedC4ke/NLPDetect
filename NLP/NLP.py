3b251605d6ccbaecaf32bac7516cc620d0a41dd3
drmixvm <drmixvm@DESKTOP-T3TQNU6>
2021-01-14 22:58:41 +0500

    Добавлена возможность обучения

--- NLP/NLP.py
+++ NLP/NLP.py
@@ -1,14 +1,3 @@
-from vectorize import Vectorize
-from pandas import read_csv as read
-from sklearn.model_selection import train_test_split as train
+from learn import Learn
 
-corpus = ['Съеж ище этех мяхких булак да выпий чаю',
-          'TF-IDF — это статистическая мера, которая позволяет определить наиболее важные слова для текста в корпусе с помощью двух параметров: частот слов в каждом документе и количества документов, содержащих определенное слово (более подробно здесь). Рассчитав для каждого слова в сообщении TF-IDF, получаем векторное представление этого сообщения. ']
-
-data = read('myds.csv', delimiter=';', encoding='ansi')
-print(data.head())
-
-X = data.values[::, 1:]
-Y = data.values[::, 0:1]
-print(X)
-print(Y)
+Learn()
--- NLP/vectorize.py
+++ NLP/vectorize.py
@@ -1,12 +1,18 @@
 #векторизация с использованием TF-IDF
 
 from sklearn.feature_extraction.text import TfidfVectorizer
+
 from scipy import sparse
 from tnize import GetTokens
 
 def Vectorize(corpus):
-    vector_words = TfidfVectorizer(max_features=10000, tokenizer=GetTokens, analyzer='word', ngram_range=(1,3))
-    vector_char = TfidfVectorizer(max_features=30000, analyzer='char', lowercase=True, ngram_range=(3,6))
-    x_words = vector_words.fit_transform(corpus)
-    x_char = vector_char.fit_transform(corpus)
-    return sparse.hstack([x_words, x_char])
+    #vector_words = TfidfVectorizer(max_features=10000, tokenizer=GetTokens, analyzer='word', ngram_range=(1,3))
+    #vector_char = TfidfVectorizer(max_features=30000, analyzer='char', lowercase=True, ngram_range=(3,6))
+    #x_words = vector_words.fit_transform(corpus)
+    #x_char = vector_char.fit_transform(corpus)
+    vector = TfidfVectorizer(tokenizer=GetTokens)
+    x_word = vector.fit_transform(corpus)
+
+    #return sparse.hstack([x_words, x_char])
+
+    return x_word
