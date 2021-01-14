62e6f9006de3afb0b95afe5692662b2c3c8bb122
drmixvm <drmixvm@DESKTOP-T3TQNU6>
2021-01-14 14:41:00 +0500

    Векторизация Nграм слов и символов

--- NLP/vectorize.py
+++ NLP/vectorize.py
@@ -1,10 +1,12 @@
 #векторизация с использованием TF-IDF
 
-from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy import sparse
 from tnize import GetTokens
 
 def Vectorize(corpus):
-    vectorizer = TfidfVectorizer(tokenizer=GetTokens)
-    X = vectorizer.fit_transform(corpus)
-    return X.toarray()
+    vector_words = TfidfVectorizer(tokenizer=GetTokens, analyzer='word', ngram_range=(1,3))
+    vector_char = TfidfVectorizer(analyzer='char', lowercase=True, ngram_range=(3,6))
+    x_words = vector_words.fit_transform(corpus)
+    x_char = vector_char.fit_transform(corpus)
+    return sparse.hstack([x_words, x_char])
